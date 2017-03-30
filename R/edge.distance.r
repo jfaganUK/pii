99f2b1b462a70f6bccee2af434727f900e0f52c4
jfaganUK <jesse.fagan@uky.edu>
2017-03-30 14:40:34 -0400

    Added a 3d lookup table option for edge distance.

--- R/edge.distance.r
+++ R/edge.distance.r
@@ -8,7 +8,7 @@
 #' @examples
 #' get.edge.network(g)
 
-edge.distance <- function(g, edge.network = NULL, add.names=T) {
+edge.distance <- function(g, edge.network = NULL, lookup.mat = F, add.names=T) {
   if(is.null(edge.network)) {
     edge.network <- get.edge.network(g)
   }
@@ -17,6 +17,17 @@ edge.distance <- function(g, edge.network = NULL, add.names=T) {
   enm <- (max.node.id + 1) : (max.node.id + ecount(g))
   g.dist <- shortest.paths(graph.edgelist(edge.network))
   g.dist <- (g.dist[enm, 1:max.node.id] - 1) / 2
+  if(lookup.mat) {
+    # 3d lookup matrix (edge node 1, edge node 2, focal node)
+    m <- array(NA, rep(vcount(g), 3))
+    g.el <- get.edgelist(g, names = F)
+    # this creates an index for the 3d matrix and merges it with the melted
+    b <- do.call(rbind, replicate(ncol(g.dist), g.el, simplify=F)) %>%
+      cbind(reshape2::melt(g.dist)) %>%
+      select(-Var1)
+    m[b %>% select(`1`, `2`, `Var2`) %>% as.matrix] <- b$value
+    return(m)
+  }
   if(add.names) {
     if("matrix" %in% class(g.dist)) {
       if(is.null(ns)) {
@@ -29,5 +40,8 @@ edge.distance <- function(g, edge.network = NULL, add.names=T) {
     g.el <- get.edgelist(g)
     rownames(g.dist) <- paste(g.el[,1],g.el[,2],sep="-")
   }
-  g.dist
+  return(g.dist)
 }
+
+
+
