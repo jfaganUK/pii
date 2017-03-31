97c6cd4b85b89f3fe4de7eca47c4f788a33dbb07
jfaganUK <jesse.fagan@uky.edu>
2017-03-31 10:49:28 -0400

    Working on improving triad table

--- piitesting/pii_scrap.R
+++ piitesting/pii_scrap.R
@@ -558,3 +558,18 @@ for(i in 1:vcount(g)){
   }
 }
 table <- table[table < -0.5 & table > -0.9]
+
+### testing multidimensional rccp arrays ###
+
+m <- edge.distance(g, lookup.mat = T)
+
+cppFunction(code = '
+void getCell(NumericVector inputEdgeDistance, IntegerVector Vcell) {
+  IntegerVector inputEdgeDistanceDims = inputEdgeDistance.attr("dim");
+  arma::cube cubeEdgeDistance(inputEdgeDistance.begin(), inputEdgeDistanceDims[0], inputEdgeDistanceDims[1], inputEdgeDistanceDims[2], false);
+  Rcout << "Hello world!: " << cubeEdgeDistance(Vcell[0]-1, Vcell[1]-1, Vcell[2]-1);
+}', depends = 'RcppArmadillo')
+
+getCell(m, c(1,2,3))
+
+
