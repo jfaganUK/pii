a44de1dbc0e0cda1359721f9e8b8cdc9bf851ffe
jesse.fagan@uky.edu <jesse.fagan@uky.edu>
2017-03-31 11:29:20 -0400

    Doing some cleaning.

--- R/triad.table.r
+++ R/triad.table.r
@@ -8,27 +8,12 @@
 #' @export
 triad.table <- function(g) {
   if(is.null(V(g)$name)){V(g)$name <- 1:vcount(g)}
-  #e.dist <- edge.distance(g)
-  node_distance_holder <- shortest.paths(g, V(g))
-  #e.dist.l <- list(ncol(e.dist))
-  #nms <- strsplit(rownames(e.dist), "-")
-  # #for(j in 1:ncol(e.dist)) {
-  #   m1 <- matrix(NA, nrow = vcount(g), ncol = vcount(g))
-  #   rownames(m1) <- colnames(e.dist)
-  #   colnames(m1) <- colnames(e.dist)
-  #
-  #   for(i in 1:nrow(e.dist)) {
-  #     x <- nms[[i]]
-  #     m1[x[1], x[2]] <- e.dist[i, j]
-  #     m1[x[2], x[1]] <- e.dist[i, j]
-  #   }
-  #   e.dist.l[[j]] <- m1
-  # }
+  n.dist <- shortest.paths(g, V(g))
   e.dist.l <- edge.distance(g, lookup.mat = T)
   triads <- cliques(g, min = 3, max= 3)
   triads <- do.call('rbind', lapply(triads, function(x) { as.integer(x) }))
   dimNum <- vcount(g)
   if(is.null(triads)){stop("There are no triads in the graph")}
-  triad_table <- triadTable(edgeDistance = e.dist.l, shortPaths = node_distance_holder, triads=triads, vertices=V(g), edgevalence = E(g)$valence)
+  triad_table <- triadTable(edgeDistance = e.dist.l, shortPaths = n.dist, triads=triads, vertices=V(g), edgevalence = E(g)$valence)
   return(triad_table)
 }
